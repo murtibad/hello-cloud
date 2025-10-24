372221e6c682cd9e3755a9e384fdab0925c55f38
murtibad <murat.16-16@hotmail.com>
2025-10-24 09:33:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def home():
+    return "Merhaba, Buluttan Selam!"
 
 if __name__ == "__main__":
     app.run(debug=True)
